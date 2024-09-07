6f34ffa1ac771b8227a84b65bca8d20935b72c02
Mohak-Royal <mohak.royal2112@gmail.com>
2024-09-07 19:59:57 +0530

    getch , clrscr , Escape Sequence

---
+++ Basics/BASIC1.C
@@ -0,0 +1,14 @@
+/*
+	Name: Royal
+	DOC : 09-06-24
+	Purpose: Basics of C
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	clrscr();
+	printf("Hello there, this is prachi here");
+	getch();
+}
---
+++ Basics/ESCAPE.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void main(){
+	clrscr();
+
+	//printf("Hello there \"mohak\"");
+
+	//printf("Hello there\nmy name is\nMohak");
+	//printf("Hello there");
+	//printf("\nmy name is Mohak");
+
+
+	printf("Hellothere\t");
+	printf("Mohak");
+
+	getch();
+}
